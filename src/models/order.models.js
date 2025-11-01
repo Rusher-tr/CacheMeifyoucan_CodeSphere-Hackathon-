d2158ef37d4679e8580b6460718b9f5ce2c360b7
Rusher-tr <syedahmerali12789@gmail.com>
2025-11-01 17:21:54 +0500

    initial work

---
+++ src/models/order.models.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const orderSchema = new Schema(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    buyerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    sellerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "Held",
+        "shipped",
+        "Completed",
+        "Disputed",
+        "Refunded",
+      ],
+      default: "pending",
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    shippingProvider: {
+      type: String,
+    },
+    trackingNumber: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
